f1735a361ef4435f8f8bbebecd26978a38353d98
magda <magdalena.aniol@gmail.com>
2018-07-16 19:29:17 +0200

    add express test app

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var fs = require('fs')
+var bodyParser = require('body-parser');
+
+var stringifyFile
+
+app.use(bodyParser.json());
+
+app.get('/getNote', function (req, res) {
+	console.log('Otrzymałem żądanie GET do strony getNote');
+	fs.readFile('./note.json', 'utf8', function(err, data) {
+		if (err) throw err;
+		stringifyFile = data;
+		res.send(data);
+	});
+});
+
+app.post('/updateNote/:note', function (req, res) {
+	console.log('Otrzymałem żądanie POST do strony getNote/:note');
+	stringifyFile += req.params.note;
+	fs.writeFile('./note.json', stringifyFile, function(err, data) {
+		if (err) throw err;
+		console.log('File updated');
+	});
+});
+
+app.use(function (req, res, next) {
+	res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
+
+app.listen(3000);
