b62be3a3aa152a93f25bacdcfce5c690b1570fb0
magda <magdalena.aniol@gmail.com>
2018-07-16 20:35:21 +0200

    add missing ;

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 app.use(express.static('assets'));
 app.get('/', function (req, res) {
-	res.sendFile('/index.html')
+	res.sendFile('/index.html');
 });
 app.get('/userform', function (req, res) {
 	const response = {
