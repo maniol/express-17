cbca74a18e7b8ce79b607dd15264122d8c3db2ce
magda <magdalena.aniol@gmail.com>
2018-07-17 18:45:46 +0200

    add middleware function

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 var express = require('express');
 var app = express();
-app.use(express.static('assets'));
+
+app.use('/store', function(req, res, next){
+	console.log('Jestem pośrednikiem przy żądaniu do /store');
+	next();
+});
+
 app.get('/', function (req, res) {
-	res.sendFile('/index.html');
-});
-app.get('/userform', function (req, res) {
-	const response = {
-		first_name: req.query.first_name,
-		last_name: req.query.last_name
-	};
-	res.end(JSON.stringify(response));
+	res.send('Hello world!');
 });
 
-var server = app.listen(3000, 'localhost', function() {
-	var host = server.address().address;
-	var port = server.address().port;
+app.get('/store', function (req, res) {
+	res.send('To jest sklep');
+});
 
-	console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
+app.listen(3000);
+app.use(function (req, res, next) {
+	res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
 });
