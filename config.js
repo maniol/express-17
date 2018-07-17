d6c7c3a4fff0d79cf60de331fb0803ec774fc487
magda <magdalena.aniol@gmail.com>
2018-07-17 21:33:30 +0200

    add oauth

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '937474605960-k0rpbk3n69i59bsujjl4fcq7bo74nt57.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'h_Aoo5BnTMUngZbnR0CSjtui',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
